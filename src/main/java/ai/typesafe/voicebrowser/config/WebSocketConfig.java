3cb0bcae0829f9c3f2cf53d74fcad68eb010e35c
Muwafaq2005 <mmrs2024coding@gmail.com>
2026-09-25 11:19:23 +0530

    fix: route WebSocket endpoint to /ws so HTTP GET / serves index.html UI

--- src/main/java/ai/typesafe/voicebrowser/config/WebSocketConfig.java
+++ src/main/java/ai/typesafe/voicebrowser/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new WebSocketsHandler(controller), "/")
+        registry.addHandler(new WebSocketsHandler(controller), "/ws", "/ws/")
                 .setAllowedOrigins("*");
     }
 }
